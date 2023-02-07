18c30d786f85b14e6f9a37cdd1c291c20a1d2a89
georgeeset <georgeperfect4u@gmail.com>
2023-02-07 13:20:12 +0100

    pycodestyle compliance done for base model

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,6 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
-
     def __str__(self):
         return "[{}] ({}) {}".format((self.__class__.__name__),
                                      self.id, self.__dict__)
