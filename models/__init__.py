70075569d2605d3d07499f6f0025c71cd3ad5608
FearGod12 <onyenikechukwudi@gmail.com>
2023-02-07 16:22:44 +0000

    task 5

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+to create a unique FileStorage instance for your application
+'''
+from models.engine import file_storage
+
+
+storage = file_storage.FileStorage()
+storage.reload()
