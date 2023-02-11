e6294c047572168a909581ad6fe29de2f6616553
georgeeset <georgeperfect4u@gmail.com>
2023-02-11 02:07:08 +0100

    => more tests added to the file_storage class => while testing the reload method, the following issues were encountered  TypeError: City.__init__() got an unexpected keyword argument 'id'  TypeError: Amenity.__init__() got an unexpected keyword argument 'id' => these errors were corrected by removing the __init__ method of the affected classes

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,3 @@ class Amenity(BaseModel):
     """ creates an instance of an Amenity """
 
     name = ""
-
-    def __init__(self):
-        """ initializes Amenity """
-        super(Amenity, self).__init__()
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,9 +5,15 @@ test m;odule for FileStorage class
 import unittest
 from datetime import datetime
 import time
-from models import storage
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 import os
 import json
 
@@ -44,14 +50,48 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(getattr(FileStorage, "_FileStorage__objects"), {})
         self.assertTrue(hasattr(FileStorage, "_FileStorage__objects"))
 
-    def test_all_1(self):
-        """test the all method using BaseModel"""
-        self.assertEqual(storage.all(), {})
-        test_class = BaseModel()
+    """================Test method AlL with Existing classes"""
+    def prepare_all_test(self, test_class):
+        """Prepare each class for the test"""
         storage.new(test_class)
         key = "{}.{}".format(type(test_class).__name__, test_class.id)
         self.assertTrue(key in storage.all())
         self.assertEqual(storage.all()[key], test_class)
+
+    def test_all_base(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(BaseModel())
+
+    def test_all_user(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(User())
+
+    def test_all_city(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(City())
+
+    def test_all_state(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(State())
+
+    def test_all_review(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(Review())
+
+    def test_all_amenity(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(Amenity())
+
+    def test_all_place(self):
+        """test the all method using BaseModel"""
+        self.assertEqual(storage.all(), {})
+        self.prepare_all_test(Place())
 
     def test_all_with_no_args(self):
         """test all method without arguments"""
@@ -61,16 +101,47 @@ class TestFileStorage(unittest.TestCase):
                "positional argument: 'self'")
         self.assertEqual(str(e.exception), msg)
 
-    def test_new_1(self):
-        """ test the new method with new BaseModel"""
-        test_class = BaseModel()
-        key = "{}.{}".format(type(test_class).__name__, test_class.id)
-        self.assertTrue(key in FileStorage._FileStorage__objects)
-        self.assertEqual(FileStorage._FileStorage__objects[key], test_class)
+    """====Test The New method in storage class with available models ======"""
 
-    def test_save_1(self):
-        """ test the save method with BaseModel"""
-        test_class = BaseModel()
+    def prepare_new_test(self, test_class):
+        """ Prepare class proparty for all models"""
+        cls = test_class
+        storage.new(cls)
+        key = "{}.{}".format(type(cls).__name__, cls.id)
+        self.assertTrue(key in FileStorage._FileStorage__objects)
+        self.assertEqual(FileStorage._FileStorage__objects[key], cls)
+
+    def test_new_base(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(BaseModel())
+
+    def test_new_User(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(User())
+
+    def test_new_state(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(State())
+
+    def test_new_place(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(Place())
+
+    def test_new_review(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(Review())
+
+    def test_new_amenity(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(Amenity())
+
+    def test_new_city(self):
+        """ test the new method with new BaseModel"""
+        self.prepare_new_test(City())
+
+    """=========Test Save method with all available models"""
+    def prepare_save_test(self, test_class):
+        """prepare test for save method"""
         storage.new(test_class)
         key = "{}.{}".format(type(test_class).__name__, test_class.id)
         storage.save()
@@ -82,13 +153,70 @@ class TestFileStorage(unittest.TestCase):
             f.seek(0)
             self.assertEqual(json.load(f), rep)
 
-    def test_reload(self):
-        """ test the reload method with Base class"""
+    def test_save_base(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(BaseModel())
+
+    def test_save_user(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(User())
+
+    def test_save_place(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(Place())
+
+    def test_save_state(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(State())
+
+    def test_save_amenity(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(Amenity())
+
+    def test_save_city(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(City())
+
+    def test_save_review(self):
+        """ test the save method with BaseModel"""
+        self.prepare_save_test(Review())
+
+    """==============Test Reload method with all models=============="""
+    def prepare_reload_test(self, class_name):
+        """PREPARE reload test for all calsses"""
         storage.reload()
         self.assertEqual(FileStorage._FileStorage__objects, {})
-        obj = BaseModel()
+        obj = class_name()
         storage.new(obj)
         key = "{}.{}".format(type(obj).__name__, obj.id)
         storage.save()
         storage.reload()
         self.assertEqual(obj.to_dict(), storage.all()[key].to_dict())
+
+    def test_reload_base(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(BaseModel)
+
+    def test_reload_user(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(User)
+
+    def test_reload_place(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(Place)
+
+    def test_reload_state(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(State)
+
+    def test_reload_amenity(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(Amenity)
+
+    def test_reload_city(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(City)
+
+    def test_reload_review(self):
+        """ test the save method with BaseModel"""
+        self.prepare_reload_test(Review)
